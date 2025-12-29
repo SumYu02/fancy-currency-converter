29a2c58f8badcbe9669a9216e4d0901d90eeb983
Wong Sum Yu <sumyu.wong@it.destijltech.com>
2025-12-29 17:26:48 +0800

    clean up the list of index

--- src/index.js
+++ src/index.js
@@ -1,137 +1,192 @@
 import imgLogo from "./demex_color.png";
 import bgCurrency from "./bg-currency.avif";
-export { imgLogo, bgCurrency };
+import logoBlur from "./tokens/BLUR.svg";
+import logoBneo from "./tokens/bNEO.svg";
+import logoBusd from "./tokens/BUSD.svg";
+import logoUsdc from "./tokens/USDC.svg";
+import logoUsd from "./tokens/USD.svg";
+import logoEth from "./tokens/ETH.svg";
+import logoGmx from "./tokens/GMX.svg";
+import logoStem from "./tokens/EVMOS.svg";
+import logoLuna from "./tokens/LUNA.svg";
+import logoRatom from "./tokens/RATOM.svg";
+import logoStrd from "./tokens/STRD.svg";
+import logoEvmos from "./tokens/EVMOS.svg";
+import logoIbcx from "./tokens/IBCX.svg";
+import logoIris from "./tokens/IRIS.svg";
+import logoAmpLuna from "./tokens/ampLUNA.svg";
+import logoKuji from "./tokens/KUJI.svg";
+import logoStosmo from "./tokens/STOSMO.svg";
+import logoAtom from "./tokens/ATOM.svg";
+import logoStatom from "./tokens/STATOM.svg";
+import logoOsmo from "./tokens/OSMO.svg";
+import logoRswth from "./tokens/rSWTH.svg";
+import logoStluna from "./tokens/STLUNA.svg";
+import logoLsi from "./tokens/LSI.svg";
+import logoOkb from "./tokens/OKB.svg";
+import logoSwth from "./tokens/SWTH.svg";
+import logoUsc from "./tokens/USC.svg";
+import logoWbtc from "./tokens/WBTC.svg";
+import logoWstEth from "./tokens/wstETH.svg";
+import logoYieldUsd from "./tokens/YieldUSD.svg";
+import logoZil from "./tokens/ZIL.svg";
 
-const currencies = [
+export const currencies = [
   {
     currency: "BLUR",
     date: "2023-08-29T07:10:40.000Z",
     price: 0.208115254237288,
+    logo: logoBlur,
   },
   {
     currency: "bNEO",
     date: "2023-08-29T07:10:50.000Z",
     price: 7.1282679,
+    logo: logoBneo,
   },
   {
     currency: "BUSD",
     date: "2023-08-29T07:10:40.000Z",
     price: 0.999183113,
+    logo: logoBusd,
   },
   {
     currency: "BUSD",
     date: "2023-08-29T07:10:40.000Z",
     price: 0.999878261118644,
+    logo: logoUsdc,
   },
   {
     currency: "USD",
     date: "2023-08-29T07:10:30.000Z",
     price: 1,
+    logo: logoUsd,
   },
   {
     currency: "ETH",
     date: "2023-08-29T07:10:52.000Z",
     price: 1645.93373737374,
+    logo: logoEth,
   },
   {
     currency: "GMX",
     date: "2023-08-29T07:10:40.000Z",
     price: 36.3451143728814,
+    logo: logoGmx,
   },
   {
     currency: "STEVMOS",
     date: "2023-08-29T07:10:40.000Z",
     price: 0.0727670677966102,
+    logo: logoStem,
   },
   {
     currency: "LUNA",
     date: "2023-08-29T07:10:40.000Z",
     price: 0.409556389830508,
+    logo: logoLuna,
   },
   {
     currency: "RATOM",
     date: "2023-08-29T07:10:40.000Z",
     price: 10.2509189152542,
+    logo: logoRatom,
   },
   {
     currency: "STRD",
     date: "2023-08-29T07:10:40.000Z",
     price: 0.738655338983051,
+    logo: logoStrd,
   },
   {
     currency: "EVMOS",
     date: "2023-08-29T07:10:40.000Z",
     price: 0.062461813559322,
+    logo: logoEvmos,
   },
   {
     currency: "IBCX",
     date: "2023-08-29T07:10:40.000Z",
     price: 41.268113559322,
+    logo: logoIbcx,
   },
   {
     currency: "IRIS",
     date: "2023-08-29T07:10:40.000Z",
     price: 0.0177095593220339,
+    logo: logoIris,
   },
   {
     currency: "ampLUNA",
     date: "2023-08-29T07:10:40.000Z",
     price: 0.495485898305085,
+    logo: logoAmpLuna,
   },
   {
     currency: "KUJI",
     date: "2023-08-29T07:10:45.000Z",
     price: 0.675,
+    logo: logoKuji,
   },
   {
     currency: "STOSMO",
     date: "2023-08-29T07:10:45.000Z",
     price: 0.431318,
+    logo: logoStosmo,
   },
   {
     currency: "USDC",
     date: "2023-08-29T07:10:40.000Z",
     price: 0.989832,
+    logo: logoUsdc,
   },
   {
     currency: "axlUSDC",
     date: "2023-08-29T07:10:40.000Z",
     price: 0.989832,
+    logo: logoAxlUsdc,
   },
   {
     currency: "ATOM",
     date: "2023-08-29T07:10:50.000Z",
     price: 7.18665733333333,
+    logo: logoAtom,
   },
   {
     currency: "STATOM",
     date: "2023-08-29T07:10:45.000Z",
     price: 8.51216205084746,
+    logo: logoStatom,
   },
   {
     currency: "OSMO",
     date: "2023-08-29T07:10:50.000Z",
     price: 0.377297433333333,
+    logo: logoOsmo,
   },
   {
     currency: "rSWTH",
     date: "2023-08-29T07:10:40.000Z",
     price: 0.00408771,
+    logo: logoRswth,
   },
   {
     currency: "STLUNA",
     date: "2023-08-29T07:10:40.000Z",
     price: 0.442322101694915,
+    logo: logoStluna,
   },
   {
     currency: "LSI",
     date: "2023-08-29T07:10:50.000Z",
     price: 67.6966152542373,
+    logo: logoLsi,
   },
   {
     currency: "OKB",
     date: "2023-08-29T07:10:40.000Z",
     price: 42.9756205932203,
+    logo: logoOkb,
   },
   {
     currency: "OKT",
@@ -142,11 +197,13 @@ const currencies = [
     currency: "SWTH",
     date: "2023-08-29T07:10:45.000Z",
     price: 0.00403985045501208,
+    logo: logoSwth,
   },
   {
     currency: "USC",
     date: "2023-08-29T07:10:40.000Z",
     price: 0.994,
+    logo: logoUsc,
   },
   {
     currency: "USDC",
@@ -162,25 +219,30 @@ const currencies = [
     currency: "USDC",
     date: "2023-08-29T07:10:40.000Z",
     price: 0.999878261118644,
+    logo: logoUsdc,
   },
   {
     currency: "WBTC",
     date: "2023-08-29T07:10:52.000Z",
     price: 26002.822020202,
+    logo: logoWbtc,
   },
   {
     currency: "wstETH",
     date: "2023-08-29T07:10:40.000Z",
     price: 1872.25797423729,
+    logo: logoWstEth,
   },
   {
     currency: "YieldUSD",
     date: "2023-08-29T07:10:40.000Z",
     price: 1.02908479661017,
+    logo: logoYieldUsd,
   },
   {
     currency: "ZIL",
     date: "2023-08-29T07:10:50.000Z",
     price: 0.0165181355932203,
+    logo: logoZil,
   },
 ];
