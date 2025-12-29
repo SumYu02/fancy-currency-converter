787e5c2a41875112dddd1668fb1020e9d9a5a147
Wong Sum Yu <sumyu.wong@it.destijltech.com>
2025-12-29 11:09:53 +0800

    clean the code structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "../pages/Home.jsx";
+import Home from "./pages/Home.jsx";
 
 const App = () => {
   return (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
+    <div >
       <h1 className="text-2xl font-bold">Home</h1>
       <form>
         <input type="text" placeholder="Name" />
