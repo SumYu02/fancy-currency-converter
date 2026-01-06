8282d1b4bbe51a2353d25a3650a6c74a4b590463
Wong Sum Yu <sumyu.wong@it.destijltech.com>
2026-01-06 14:21:02 +0800

    Add @vercel/analytics package for real-time traffic insights; update App component to include Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Analytics />
+    </>
   );
 };
 
